c9bde1bd8cf249919e5a313fce1c5d72fdaf5959
trdmitr <trdmitr@gmail.com>
2020-12-09 23:57:21 +0400

    progress bar

--- script.js
+++ script.js
@@ -3,6 +3,35 @@ const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
 
+const image = document.querySelector('img')
+const title = document.getElementById('title')
+const artist = document.getElementById('artist')
+
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
+
+const currentTimeEl = document.getElementById('current-time');
+const durationEl = document.getElementById('duration');
+
+//Музыка 
+const songs = [
+    {
+        name: 'jacinto-1',
+        displayName: 'Super mashin',
+        artist: 'Jacinto'
+    },
+    {
+        name: 'jacinto-2',
+        displayName: 'Super mashin2',
+        artist: 'Jacinto'
+    },
+    {
+        name: 'jacinto-3',
+        displayName: 'Super mashin3',
+        artist: 'Jacinto'
+    },
+];
+
 //проверка play
 let isPlaying = false;
 
@@ -24,3 +53,70 @@ function pauseSong() {
 
 // play pauser Event Listener
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+ // вывод в DOM
+ function loadSong(song) {
+     title.textContent = song.displayName;
+     artist.textContent = song.artist;
+     music.src = `music/${song.name}.mp3`;
+     image.src = `img/${song.name}.jpg`
+ }
+  // текущая песня
+ let songIndex = 0;
+//prev song
+ function prevSong() {
+     songIndex--;
+     if (songIndex < 0) {
+         songIndex = songs.length -1;
+     }
+     console.log(songIndex)
+     loadSong(songs[songIndex]);
+     playSong();
+ }
+//next song
+
+function nextSong() {
+    songIndex++;
+    if (songIndex > songs.length -1) {
+        songIndex = 0;
+    }
+    console.log(songIndex)
+    loadSong(songs[songIndex]);
+    playSong();
+
+}
+ // Выбор пестни
+ loadSong(songs[songIndex]);
+
+ function updateProgressBar(e) {
+     if (isPlaying) {
+         const {duration, currentTime} = e.srcElement;
+         
+         const progressPercent = (currentTime / duration) * 100;
+         progress.style.width = `${progressPercent}%`;
+
+         const durationMinutes = Math.floor(duration / 60);
+         console.log ('minut: ', durationMinutes);
+         let durationSeconds = Math.floor(duration % 60);
+         if (durationSeconds < 10) {
+             durationSeconds = `0${durationSeconds}`;
+         }
+         console.log('second: ', durationSeconds);
+         
+         if(durationSeconds) {
+           durationEl.textContent = `${durationMinutes}:${durationSeconds}`  
+         }
+        //ddD
+        const currentMinutes = Math.floor(currentTime / 60);
+        console.log ('minut: ', currentMinutes);
+        let currentSeconds = Math.floor(currentTime % 60);
+        if (currentSeconds < 10) {
+            currentSeconds = `0${currentSeconds}`;
+        }
+        console.log('second: ', currentSeconds);
+        currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
+     }
+ }
+
+ prevBtn.addEventListener('click', prevSong);
+ nextBtn.addEventListener('click', nextSong);
+ music.addEventListener('timeupdate', updateProgressBar);
