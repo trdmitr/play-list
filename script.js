d7cb57b758e6f79b7a17dd479242b2504b6e59dd
trdmitr <trdmitr@gmail.com>
2020-12-10 10:13:26 +0400

    finish

--- script.js
+++ script.js
@@ -28,8 +28,13 @@ const songs = [
     {
         name: 'jacinto-3',
         displayName: 'Super mashin3',
-        artist: 'Jacinto'
+        artist: 'Jacinto',
     },
+    {
+        name: 'metric-1',
+        displayName: 'Front Row (Remix)',
+        artist: 'Metric/Jacinto Design',
+      },
 ];
 
 //проверка play
@@ -38,16 +43,16 @@ let isPlaying = false;
 // Play
 function playSong() {
     isPlaying = true;
-    playBtn.classList.replace('fa-play', 'fa-pause')
-    playBtn.setAttribute('title', 'Pause')
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    playBtn.setAttribute('title', 'Pause');
     music.play();
 }
 
 //pause
 function pauseSong() {
     isPlaying = false;
-    playBtn.classList.replace('fa-pause', 'fa-play')
-    playBtn.setAttribute('title', 'Play')
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
     music.pause();
 }
 
@@ -58,7 +63,7 @@ playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
      title.textContent = song.displayName;
      artist.textContent = song.artist;
      music.src = `music/${song.name}.mp3`;
-     image.src = `img/${song.name}.jpg`
+     image.src = `img/${song.name}.jpg`;
  }
   // текущая песня
  let songIndex = 0;
@@ -66,9 +71,9 @@ playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
  function prevSong() {
      songIndex--;
      if (songIndex < 0) {
-         songIndex = songs.length -1;
+         songIndex = songs.length - 1;
      }
-     console.log(songIndex)
+    //  console.log(songIndex)
      loadSong(songs[songIndex]);
      playSong();
  }
@@ -76,10 +81,10 @@ playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
 
 function nextSong() {
     songIndex++;
-    if (songIndex > songs.length -1) {
+    if (songIndex > songs.length - 1) {
         songIndex = 0;
     }
-    console.log(songIndex)
+    //console.log(songIndex)
     loadSong(songs[songIndex]);
     playSong();
 
@@ -89,34 +94,45 @@ function nextSong() {
 
  function updateProgressBar(e) {
      if (isPlaying) {
-         const {duration, currentTime} = e.srcElement;
+        const { duration, currentTime } = e.srcElement;
          
          const progressPercent = (currentTime / duration) * 100;
          progress.style.width = `${progressPercent}%`;
 
          const durationMinutes = Math.floor(duration / 60);
-         console.log ('minut: ', durationMinutes);
+        //  console.log ('minut: ', durationMinutes);
          let durationSeconds = Math.floor(duration % 60);
          if (durationSeconds < 10) {
              durationSeconds = `0${durationSeconds}`;
          }
-         console.log('second: ', durationSeconds);
+        //  console.log('second: ', durationSeconds);
          
-         if(durationSeconds) {
-           durationEl.textContent = `${durationMinutes}:${durationSeconds}`  
+         if (durationSeconds) {
+           durationEl.textContent = `${durationMinutes}:${durationSeconds}`; 
          }
         //ddD
         const currentMinutes = Math.floor(currentTime / 60);
-        console.log ('minut: ', currentMinutes);
+        // console.log ('minut: ', currentMinutes);
         let currentSeconds = Math.floor(currentTime % 60);
         if (currentSeconds < 10) {
             currentSeconds = `0${currentSeconds}`;
         }
-        console.log('second: ', currentSeconds);
+        // console.log('second: ', currentSeconds);
         currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
      }
  }
 
- prevBtn.addEventListener('click', prevSong);
- nextBtn.addEventListener('click', nextSong);
- music.addEventListener('timeupdate', updateProgressBar);
+function setProgressBar(e) {
+    // console.log(e)
+    const width = this.clientWidth;
+    // console.log('width - ', width);
+    const clickX = e.offsetX;
+    const { duration } = music;
+    music.currentTime = (clickX / width) * duration;
+}
+
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+music.addEventListener('ended', nextSong);
+music.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', setProgressBar);
